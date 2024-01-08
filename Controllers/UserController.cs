2b481a28db24acd73843db2e3e795dff44f42c19
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-01-08 14:16:47 +0300

    Add user controller.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using EverydayIsArtAPI.Models;
+using EverydayIsArtAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EverydayIsArtAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public UserController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            string response = await _authenticationService.Login(request);
+            return Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            string response = await _authenticationService.Register(request);
+            return Ok(response);
+        }
+    }
+}
