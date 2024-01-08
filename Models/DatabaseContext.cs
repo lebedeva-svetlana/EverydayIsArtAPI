0fe790693573adb1f1b4cd9ecbc65dd1cde555e7
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-01-08 14:07:56 +0300

    Add database context.

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EverydayIsArtAPI.Models
+{
+    public class DatabaseContext : IdentityDbContext<User>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+    }
+}
