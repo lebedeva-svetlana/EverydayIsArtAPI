0e10f0dfdcdc45cc594b1ba7b41cd0b667f15c81
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-06-29 19:01:50 +0300

    Comment UseHttpsRedirection.

--- EverydayIsArtAPI/Program.cs
+++ EverydayIsArtAPI/Program.cs
@@ -111,7 +111,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(reactClient);
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
